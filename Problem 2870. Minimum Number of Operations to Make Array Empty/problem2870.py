1bbf36d333f4d1dee7e61994047c9954bf7037f3
Oleg Ivanov <78684921+ivanovsdesign@users.noreply.github.com>
2024-01-05 00:28:58 +0300

    Added incomplete solution to problem 2870

--- Problem 2870. Minimum Number of Operations to Make Array Empty/problem2870.py
+++ Problem 2870. Minimum Number of Operations to Make Array Empty/problem2870.py
@@ -9,31 +9,63 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        fmap = []
 
         nums.sort()
 
-        n = len(nums)
+        logging.debug(f"nums_sorted: {nums}")
+
         search = []
         indeces = []
-        k = 0
+        takes = 0
+        current_num = 0
 
         for num in nums:
             logging.debug(f"External for cycle, num: {num}")
+
+
+            if num != current_num:
+                for i, nnum in enumerate(nums):
+                    logging.debug(f"Internal for cycle, num: {num}")
+                    logging.debug(f"Internal for cycle, num: {num} ; i: {i}; nums[i]: {nums[i]}; nnum: {nnum}")
+                    if nnum == num:
+                        search.append(i)
+                if len(search) % 2 == 0 or len(search) % 3 == 0:
+                    indeces.append(search)
+                    current_num = num
+            
             search = []
-            for i, nnum in enumerate(nums):
-                logging.debug(f"Internal for cycle, num: {num}")
-                logging.debug(f"Internal for cycle, num: {num} ; i: {i}; nums[i]: {nums[i]}; nnum: {nnum}")
-                if nnum == num:
-                     search.append(i)
-            if len(search) == 2 or len(search) == 3:
-                indeces.append(search)
-        
-        return indeces
+
+        search = []
+
+        for idxs in indeces:
+            for idx in idxs:
+                search.append(idx)
+
+        if len(search) == len(nums):
+            for idxs in indeces:
+                logging.debug(f"idxs len: {len(idxs)}")
+                if len(idxs) % 2 == 0:
+                    logging.debug(f"idxs % 2: {len(idxs) / 2}")
+                    takes += int((len(idxs) / 2))
+                elif len(idxs) % 3 == 0:
+                    logging.debug(f"idxs % 3: {len(idxs) / 3}")
+                    takes += int((len(idxs) / 3))
+            for i in sorted(search, reverse=True):
+                del nums[i]
+        else:
+            takes = -1
+
+        logging.debug(f"nums: {nums}")
+        logging.debug(f"search: {search}")
+        logging.debug(f"indeces: {indeces}")
+
+        return takes
 
 
 sol = Solution()
 
 nums = [2,3,3,2,2,4,2,3,4]
+#nums = [2,1,2,2,3,3]
+nums = [14,12,14,14,12,14,14,12,12,12,12,14,14,12,14,14,14,12,12]
 
 print(sol.minOperations(nums))
