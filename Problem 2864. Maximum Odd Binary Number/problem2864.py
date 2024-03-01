6c5d7ca14d8a9f98938976624bf4348d7a0be907
ivanovsdesign <78684921+ivanovsdesign@users.noreply.github.com>
2024-03-01 10:32:51 +0300

    Updated problem 2864 (added neetcode solution)

--- Problem 2864. Maximum Odd Binary Number/problem2864.py
+++ Problem 2864. Maximum Odd Binary Number/problem2864.py
@@ -25,6 +25,21 @@ from collections import deque
 class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
         
+        s = [c for c in s]
+        left = 0
+
+        for i in range(len(s)):
+            if s[i] == '1':
+                s[i], s[left] = s[left], s[i]
+                left += 1
+        s[left - 1], s[len(s) - 1] = s[len(s) - 1], s[left-1]
+
+        return ''.join(s)
+
+
+    
+    def maximumOddBinaryNumberNaive(self, s: str) -> str:
+    
         ones = []
         one = '0'
         zeros = []
