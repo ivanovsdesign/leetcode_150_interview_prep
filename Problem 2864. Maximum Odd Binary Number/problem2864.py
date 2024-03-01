7bc23dca45ea7ee58de2461684ab06506010ba51
ivanovsdesign <78684921+ivanovsdesign@users.noreply.github.com>
2024-03-01 08:31:57 +0300

    Added solution to problem 2864

---
+++ Problem 2864. Maximum Odd Binary Number/problem2864.py
@@ -0,0 +1,49 @@
+'''
+You are given a binary string s that contains at least one '1'.
+
+You have to rearrange the bits in such a way that the resulting binary number is the maximum odd binary number that can be created from this combination.
+
+Return a string representing the maximum odd binary number that can be created from the given combination.
+
+Note that the resulting string can have leading zeros.
+
+ 
+
+Example 1:
+
+Input: s = "010"
+Output: "001"
+Explanation: Because there is just one '1', it must be in the last position. So the answer is "001".
+Example 2:
+
+Input: s = "0101"
+Output: "1001"
+Explanation: One of the '1's must be in the last position. The maximum number that can be made with the remaining digits is "100". So the answer is "1001".
+'''
+from collections import deque
+
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        
+        ones = []
+        one = '0'
+        zeros = []
+
+        for i in s:
+            if i == '1' and one == '0':
+                one = '1'
+                continue
+            if i == '1' and one == '1':
+                ones.append(i)
+            if i == '0':
+                zeros.append(i)
+                
+
+        return ''.join(ones + zeros + [one])
+
+solution = Solution()
+
+s = "010"
+s = "0101"
+
+print(solution.maximumOddBinaryNumber(s))
